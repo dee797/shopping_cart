76020bfdf8a832ff9af5656093007fb000c8b205
dee797 <178827694+dee797@users.noreply.github.com>
2024-12-04 15:52:13 -0700

    Use product attributes for card info

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <div className="container">
-      <div>
-        <img src={product.image} width="100px" height="100px"/>
+      <div className="bg-gray-50 min-w-60 min-h-60 flex flex-col">
+        <img src={product.image} width="200px" height="200px" className="aspect-square w-full"/>
         <div>{product.title}</div>
-        <div>{product.price}</div>
+        <div>Price: ${product.price}</div>
       </div>
     </div>
   );
